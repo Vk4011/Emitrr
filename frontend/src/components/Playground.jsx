52b6893ad67c1351b2973840f6a3c0b1a3d8ba86
Vk4011 <gvikramram63@gmail.com>
2024-03-13 09:08:13 +0530

    frontend v1

--- frontend/src/components/Playground.jsx
+++ frontend/src/components/Playground.jsx
@@ -3,13 +3,12 @@ import "./Playground.css";
 
 function Playground() {
   const [deck, setDeck] = useState([]);
-  const [frontCard, setFrontCard] = useState(4); 
+  const [frontCard, setFrontCard] = useState(4);
   const cardTypes = ["😼", "💣", "🙅‍♂️", "🔀"];
-
-
+  const [defuserCount, SetDefusserCount] = useState(0);
   useEffect(() => {
     generateDeck();
-  }, []); 
+  }, []);
 
   const generateDeck = () => {
     let newDeck = [];
@@ -25,52 +24,76 @@ function Playground() {
     return cardTypes[index];
   };
 
- 
-
   const removeCard = () => {
     let inHandCard = deck[frontCard];
 
     switch (inHandCard) {
       case "💣":
-        console.log(`\n\t BOOM : 💣 \n`);
-        console.log("Game over");
-        
+        if (defuserCount <= 0) {
+          console.log(`\n\t BOOM : 💣 \n`);
+          console.log("Game over");
+        } else {
+          SetDefusserCount((p) => {
+            p - 1;
+          });
+          console.log("bomb diffused..");
+          console.log("Card drawn:", inHandCard);
+          console.log(`\n\t[ ${deck.slice(0, -1)} ]\n`);
+          setDeck((prevDeck) => prevDeck.slice(0, -1));
+          setFrontCard((prevFrontCard) => prevFrontCard - 1);
+          if (deck.length === 1) {
+            console.log("\n\t You win the game!");
+          }
+        }
         break;
       case "🔀":
         console.log("Shuffling...");
-        console.log(`\n\t old Deck :[${deck.slice(0,-1)}]`);
-        console.log("\n\t New Deck ")
-        generateDeck(); 
-        
+        console.log(`\n\t old Deck :[${deck.slice(0, -1)}]`);
+        console.log("\n\t New Deck ");
+        generateDeck();
         setFrontCard(4);
-       
-      
+        break;
+      case "🙅‍♂️":
+        SetDefusserCount((p) => {
+          p + 1;
+        });
+        console.log("Defuser drawn:", inHandCard);
+        console.log("\n\t Checking next card...");
+
+        console.log("\n\t Game continues.");
+        console.log(`\n\t[ ${deck.slice(0, -1)} ]\n`);
+        setDeck((prevDeck) => prevDeck.slice(0, -1));
+        setFrontCard((prevFrontCard) => prevFrontCard - 1);
+
         break;
       default:
         console.log("Card drawn:", inHandCard);
         console.log(`\n\t[ ${deck.slice(0, -1)} ]\n`);
         setDeck((prevDeck) => prevDeck.slice(0, -1));
         setFrontCard((prevFrontCard) => prevFrontCard - 1);
+        if (deck.length === 1) {
+          console.log("\n\t You win the game!");
+        }
         break;
     }
   };
 
   return (
     <>
-      <div className="card-wrapper">
-        <ul className="card-list">
-          {deck.map((card, index) => (
-            <li className="card-list__item" key={index}>
-              <div className="card">{card}</div>
-            </li>
-          ))}
-        </ul>
+      <div className="container">
+        <div className="card-wrapper">
+          <ul className="card-list">
+            {deck.map((card, index) => (
+              <li className="card-list__item }" key={index}>
+                <div className="card">{card}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button onClick={removeCard} className="button btn-shuffle">
+          Draw Card
+        </button>
       </div>
-      <br />
-      <br />
-      <button onClick={removeCard} className="button btn-shuffle">
-        Draw Card
-      </button>
     </>
   );
 }
